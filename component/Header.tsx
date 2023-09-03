c5389554dd13a6dc92429696e46a129541ec4c14
franc_mauric1 <franc_mauric1@hotmail.com>
2023-09-03 17:27:12 +0200

    creando componentes vacios

---
+++ component/Header.tsx
@@ -0,0 +1,6 @@
+export default function Header () {
+
+    return (
+        <h1>Header de momento en contruccion.. </h1>
+    )
+}
---
+++ component/Main.tsx
@@ -0,0 +1,8 @@
+export default function Main () {
+
+    return(
+        <div>
+            <h1>main cuerpo etx</h1>
+        </div>
+    )
+}
