6ace998b2e0fc7e0d7802de15aadb40cb2bac05c
franc_mauric1 <franc_mauric1@hotmail.com>
2023-09-10 22:16:32 +0200

    nav bar

--- component/NavBar.tsx
+++ component/NavBar.tsx
@@ -3,7 +3,14 @@ export default function NavBar () {
     return(
         <div>
             <div>
-                <h1>Home</h1>
+                <h1></h1>
+            </div>
+            <div>
+                <a href="">Home</a>
+                <a href="">Ristorante</a>
+                <a href="">Bar</a>
+                <a href="">Pizzeria</a>
+                <a href="">prenotaciones</a>
             </div>
         </div>
     )
