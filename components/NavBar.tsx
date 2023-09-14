c58358f32fdfd49c56e6301ff74657bbe2a4ea06
franc_mauric1 <franc_mauric1@hotmail.com>
2023-09-14 22:28:04 +0200

    usando el componente como client para que funcione el handlescroll

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,31 @@
+'use client'
+import { useState, useEffect } from "react";
+
 export default function NavBar () {
+    const [navbarVisible, setNavbarVisible] = useState(true);
+
+    const handleScroll = () => {
+        if(window.scrollY > 100) {
+            setNavbarVisible(false);
+        } else {
+            setNavbarVisible(true);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
+
 
     return(
         <div>
             <div>
                 <h1 className="">Pizzeria </h1>
             </div>
-            <div className="flex flex-wrap bg-gray-300 z-10 text-2xl justify-around w-full h-20 ">
+            <div className={`flex flex-wrap fixed top-0 left-0 w-full transition-all duration-300 bg-gray-600 opacity-900 text-2xl justify-around ${navbarVisible ? 'opacity-100' : 'opacity-0'}  h-20 `}>
                 <a href="" className="p-3 text-red-500 cursor-pointer ">Home</a>
                 <a href="" className="p-3 text-red-500 cursor-pointer ">Ristorante</a>
                 <a href="" className="p-3 text-red-500 cursor-pointer ">Bar</a>
