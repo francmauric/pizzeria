c7bf89f94fb9d6355836c41ce44c9c9c23603cd4
franc_mauric1 <franc_mauric1@hotmail.com>
2023-10-30 21:20:57 +0100

    ---

--- components/Main.tsx
+++ components/Main.tsx
@@ -29,12 +29,30 @@ export default function Main () {
           </a>
         </div>
       </div> */}
-        <div className="bg-orange-800 flex ">
+        <div className="bg-orange-800 flex flex-col ">
           <h1 className="flex">Esto es el main </h1>
           <p className="flex">Da sempre noi di Zenzerø diamo il nostro contributo a sostegno dell’ambiente, 
             privilegiando prodotti biologici e a KM0, stando attenti alla provenienza e al
              confezionamento dei prodotti evitando il packaging in plastica e tutto quello 
              che è preconfezionato, scegliendo cibi freschi e di stagione.</p>
+             <div className="flex flex-wrap w-80% ">
+                <div className="flex ">
+                  <img className="w-20" src="images/info.png" alt="" />
+                  <h3>rispetto ambientale</h3>
+                </div>
+                <div>
+                  <img className="w-20" src="images/info.png" alt="" />
+                  <h3>accurata ricerca</h3>
+                </div>
+                <div>
+                  <img className="w-20" src="images/info.png" alt="" />
+                  <h3>inclusivita</h3>
+                </div>
+                <div>
+                  <img className="w-20" src="images/info.png" alt="" />
+                  <h3>attenzione a intolleranze</h3>
+                </div>
+             </div>
         </div>
         </div>
     )
