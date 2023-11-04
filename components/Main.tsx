9cf8752565a8e0688be8cf9a01d09a7402be8450
franc_mauric1 <franc_mauric1@hotmail.com>
2023-11-04 21:38:30 +0100

    agregado d imagen en el main

--- components/Main.tsx
+++ components/Main.tsx
@@ -30,11 +30,14 @@ export default function Main () {
         </div>
       </div> */}
         <div className="bg-orange-800 flex flex-col ">
-          <h1 className="flex justify-center text-5xl pt-20">The Tower </h1>
-          <p className="flex justify-center text-center text-2xl m-28">Da sempre noi di The Tower diamo il nostro contributo a sostegno dell’ambiente, 
+          <h1 className="flex justify-center text-5xl pt-20 text-zinc-50">The Tower </h1>
+          <div className="flex w-full">
+           <p className="flex justify-center text-center text-2xl m-28 ">Da sempre noi di The Tower diamo il nostro contributo a sostegno dell’ambiente, 
             privilegiando prodotti biologici e a KM0, stando attenti alla provenienza e al
              confezionamento dei prodotti evitando il packaging in plastica e tutto quello 
              che è preconfezionato, scegliendo cibi freschi e di stagione.</p>
+             <img className="w-5/12 rounded-lg m-5" src="images/pizzeria-main.jpg" alt="" />
+          </div>  
              <div className="flex flex-wrap justify-around w-80% pb-20 ">
                 <div className="m-5 ">
                   <img className="w-20 m-5 rounded-full bg-white" src="images/cocina.png" alt="" />
