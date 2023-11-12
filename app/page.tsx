a3e326d7346f6c55f6b4fe5ffb8b3d46890729ae
franc_mauric1 <franc_mauric1@hotmail.com>
2023-11-12 19:29:56 +0100

    ----

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,7 @@ export default function Home() {
     <div>
       <Header />
       <Main />
-    <h1>empeznado pagina</h1>
-    <h2>asdfads</h2>
-    <div>dsafasdfadf</div>
+ 
     </div>
 
     
--- components/About.tsx
+++ components/About.tsx
@@ -4,8 +4,8 @@ export default function About () {
         <div className="flex ">
             <img src="images/ardente1.jpg" alt="" />
             <div className="m-3">
-                <h1 className="p-4">Chi Siamo noi</h1>
-                <p className="p-4">Hace décadas, en el corazón de la pintoresca ciudad de Nápoles, nació una pizzería que pronto se convirtió en una leyenda: La Trattoria del Sapore Italiano. Fundada por el apasionado chef italiano, Antonio Rossi, este acogedor rincón comenzó como un modesto puesto callejero, pero rápidamente se ganó el corazón de los amantes de la auténtica pizza italiana.
+                <h1 className="p-4 text-2xl text-white">Chi Siamo noi</h1>
+                <p className="p-4 text-xl text-white">Hace décadas, en el corazón de la pintoresca ciudad de Nápoles, nació una pizzería que pronto se convirtió en una leyenda: La Trattoria del Sapore Italiano. Fundada por el apasionado chef italiano, Antonio Rossi, este acogedor rincón comenzó como un modesto puesto callejero, pero rápidamente se ganó el corazón de los amantes de la auténtica pizza italiana.
 
 La historia de nuestra pizzería está entrelazada con la rica tradición culinaria de Italia. Cada receta ha sido transmitida de generación en generación, guardando celosamente los secretos que hacen que nuestras pizzas sean únicas en sabor y textura.
 
