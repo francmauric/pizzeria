08390fe6f9bf610c96e976e4ea8a579e8951a4d8
franc_mauric1 <franc_mauric1@hotmail.com>
2024-01-04 10:33:26 +0100

    ways to fix it LINK

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link"
 import NavBar from "./NavBar"
 import NavBarMobile from "./NavBarMobile"
+Link
 
 export default function Header () {
 
@@ -9,6 +11,9 @@ export default function Header () {
                 <NavBar />
                 <NavBarMobile />
             </div>
+            <Link href='/Ristorante' > 
+            <a>ristorannnnte</a>
+            </Link>
             <div className="flex object-contain relative overflow-hidden">
                 <video muted autoPlay loop className="w-full relative z-10 absolute inset-0 object-cover"  src="images/pizzavideo.mp4"></video>
             </div>
