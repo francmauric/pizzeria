79330a726779c788a417a290bec5a1617e06b7cf
franc_mauric1 <franc_mauric1@hotmail.com>
2024-01-05 10:25:56 +0100

    importando navbar

--- pages/Ristorante.tsx
+++ pages/Ristorante.tsx
@@ -1,7 +1,11 @@
+"use client"
+import NavBar from "@/components/NavBar"
+
 export default function ristorante() {
 
     return (
         <div>
+        <NavBar />
         <div>ristorante</div>
         <div>hola esta pagina es ristorante</div>
         </div>
