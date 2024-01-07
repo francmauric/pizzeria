996340e0918f67c28c987690c1b326abc8da7230
franc_mauric1 <franc_mauric1@hotmail.com>
2024-01-07 10:46:26 +0100

    arreglando page ristorante, el navbar no se importa bien

--- pages/Ristorante.tsx
+++ pages/Ristorante.tsx
@@ -5,14 +5,19 @@ export default function ristorante() {
 
     return (
         <div>
+            <div className="w-full">
             <div className="flex flex-col relative">
-            <div className="flex  relative z-20">
+                <div className="flex  relative z-20">
                 <NavBar />
                 
+                </div>
+                <div className="flex object-contain relative overflow-hidden">
+                    <img className="w-full relative z-10 absolute inset-0 object-cover" src="images/tavolo.jpeg" alt="tavolo" />
+                </div>
             </div>
             </div>
-        <div>ristorante</div>
-        <div>hola esta pagina es ristorante</div>
+        {/* <div>ristorante</div>
+        <div>hola esta pagina es ristorante</div> */}
         </div>
         )
 }
