6bb350c81104cee37aa18427d420b0db4f6db364
franc_mauric1 <franc_mauric1@hotmail.com>
2024-01-26 10:41:37 +0100

    modificando page/bar

--- pages/Bar.tsx
+++ pages/Bar.tsx
@@ -24,22 +24,28 @@ export default function bar () {
                 <Banner />
             </div>
             <main className="w-full relative ">
-            <img className=" w-full h-96 object-cover absolute inset-0" src="images/fondo-madera.jpg" alt="madera" />
+            <img className=" w-full object-cover absolute inset-0" src="images/fondo-madera.jpg" alt="madera" />
                 <div className='relative z-10'>
-                    <div className='w-full h-10 '>
-                        <h1 className='text-4xl text-white text-center'>Menu</h1>
+                    <div className='w-full  '>
+                        <h1 className='text-4xl text-white text-center p-5'>Menu</h1>
                     </div>
-                    <div className='w-full '>
-                        <div>
-                             <h2 className='text-3xl text-white '>lista di cocktails</h2>
-                        </div>
-                        <div>
-                            <ul className='text-white'>
-                                <li>Negroni</li>
-                                <li>Margarita</li>
-                                <li>Piña colada</li>
-                                <li></li>
-                            </ul>
+                    <div className='w-full flex flex-row-reverse'>
+                        <div className='w-2/4'>
+                            <div>
+                                <h2 className='text-3xl text-white '>lista di cocktails</h2>
+                            </div>
+                            <div>
+                                <ul className='text-white'>
+                                    <li>Negroni</li>
+                                    <li>Margarita</li>
+                                    <li>Piña colada</li>
+                                    <li>Mojito</li>
+                                </ul>
+                            </div>
+                        </div> 
+                        <div className='w-2/4 h-56'>
+                            <h1 className='text-white'>aqui va la imagen</h1>
+                            <img src="" alt="" />
                         </div>
                     </div>
                 </div>
