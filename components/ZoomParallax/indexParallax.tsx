b7ecfdd26f739d44d7378d9b1438ebe0a678e0ff
franc_mauric1 <franc_mauric1@hotmail.com>
2024-04-25 16:57:58 +0200

    comprendiendo parallax, probando scale de la imagen central

--- components/ZoomParallax/indexParallax.tsx
+++ components/ZoomParallax/indexParallax.tsx
@@ -1,3 +1,5 @@
+'use client'; 
+
 import styles from './styles.module.scss'
 import gatito from '../../public/images/imagenParallax/gatito.jpg'
 import hoja from '../../public/images/imagenParallax/hoja.jpg'
@@ -7,23 +9,31 @@ import monito from '../../public/images/imagenParallax/monito.jpg'
 import chiquita from '../../public/images/imagenParallax/chiquita.jpg'
 import programadora from '../../public/images/imagenParallax/programadora.jpg'
 import Image from 'next/image'
-
+import { useScroll, useTransform, motion } from 'framer-motion'
+import { useRef } from 'react';
 
 export default function IndexParallax() {
     
+    const container = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ['start start', 'end end']
+    })
     
+    const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4])
+
     return (
-        <div className={styles.container}>
+        <div ref={container} className={styles.container}>
             <div className={styles.sticky}>
                 <div className={styles.el}>
-                    <div className={styles.imageContainer}>
+                    <motion.div style={{scale: scale4}} className={styles.imageContainer}>
                         <Image 
                             src={monito}
                             fill
                             alt='image'
                             placeholder='blur'
                         />
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
