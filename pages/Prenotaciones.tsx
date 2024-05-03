57ba81d46f41d3021e8180e6c132ba126f690f15
franc_mauric1 <franc_mauric1@hotmail.com>
2024-05-03 16:30:32 +0200

    empezando el formulario en prenotaciones

--- pages/Prenotaciones.tsx
+++ pages/Prenotaciones.tsx
@@ -1,4 +1,4 @@
-/* import '@/style/globals.css'
+import '@/style/globals.css'
 
 
 import Link from "next/link"
@@ -13,79 +13,22 @@ export default function prenotaciones () {
         <div>
             
             <Header />
+            <main className='bg-gray-400'>
+                <h1>apuntamento</h1>
+
+                <form action="" className='flex flex-wrap'>
+                    <label htmlFor="">Nombre</label>
+                    <input type="text" className='bg-orange-400'/>
+                    <input type="text" />
+
+                </form>
+            </main>
             <h1 className='text-black  md:text-8xl'>PRENOTACIONES</h1>
             <Main />
             
            
         </div>
     )
-} */
-'use client'
-import styles from './ristorante.module.scss'
-import "@/style/ristorante.css"
-import "@/style/globals.css";
-import NavBar from "@/components/NavBar";
-import Footer from "@/components/Footer";
-import NavBarMobile from "@/components/NavBarMobile";
-import Banner from "@/components/Banner";
-import IndexParallax from "@/components/ZoomParallax/indexParallax"
-import { useEffect  } from 'react';
-import Lenis from '@studio-freight/lenis';
-import IndexParallax2 from '@/components/ZoomParallax/indexParallax2';
-import IndexParallax3 from '@/components/ZoomParallax/indexParallax3'
-
-export default function ristorante() {
-  
-  useEffect(() => {
-    const lenis = new Lenis()
-
-    lenis.on('scroll', (e) => {
-      console.log(e)
-    })
-
-      function raf(time) {
-          lenis.raf(time)
-          requestAnimationFrame(raf)
-      }
-      requestAnimationFrame(raf)
-  },[])
-  
-  return (
-    <div>
-      <div className="w-full ">
-        <div className="flex flex-col relative">
-          <div className="flex  relative z-20 ">
-            <NavBar />
-            <NavBarMobile />
-          </div>
-          <div className="flex object-contain relative overflow-hidden">
-            <img
-              className="w-full relative z-10 absolute inset-0 object-cover"
-              src="images/tavolo.jpeg"
-              alt="tavolo"
-            />
-          </div>
-        </div>
-      </div>
-     
-      <Banner />
-     
-      <main className=' bg-gray-400'>
-        {/* vista escritorio */}
-        <h1 className='text-4xl text-center'>Menu</h1>
-        
-      <div className={styles.main}>
-        <IndexParallax />
-        <IndexParallax2 />
-        <IndexParallax3 />
-      </div>
-        {/* vista responsive */}
-        <div></div>
-      </main>
-      <Footer />
-    </div>
-  );
 }
-
 
 
--- pages/Ristorante.tsx
+++ pages/Ristorante.tsx
@@ -1,13 +1,33 @@
+
+'use client'
+import styles from './ristorante.module.scss'
 import "@/style/ristorante.css"
 import "@/style/globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import NavBarMobile from "@/components/NavBarMobile";
 import Banner from "@/components/Banner";
+import IndexParallax from "@/components/ZoomParallax/indexParallax"
+import { useEffect  } from 'react';
+import Lenis from '@studio-freight/lenis';
+import IndexParallax2 from '@/components/ZoomParallax/indexParallax2';
+import IndexParallax3 from '@/components/ZoomParallax/indexParallax3'
 
 export default function ristorante() {
   
+  useEffect(() => {
+    const lenis = new Lenis()
 
+    lenis.on('scroll', (e) => {
+      console.log(e)
+    })
+
+      function raf(time) {
+          lenis.raf(time)
+          requestAnimationFrame(raf)
+      }
+      requestAnimationFrame(raf)
+  },[])
   
   return (
     <div>
@@ -29,62 +49,25 @@ export default function ristorante() {
      
       <Banner />
      
-      <main className='mt-20'>
+      <main className=' bg-gray-400'>
         {/* vista escritorio */}
-        <div className="bg-gray-400">
-          <h1 className="p-2 text-3xl bg-white">Menu</h1>
-          <div className="bg-green-200 p-2">
-            <div className="bg-gray-200">
-              <h2 className="p-2 text-xl">Primi Piatti</h2>
-              <ol>
-                <li>Spaghetti alle volgole</li>
-                <li>Spaghetti ai Frutti di mare</li>
-                <li>Tagliolini all'Astice</li>
-                <li>Tagliolini al Salmone</li>
-                <li>Gnocchi Zucchine e Scampi</li>
-                <li>Ravioli al Salmone</li>
-                <li>Risotto ai Frutti di mare</li>
-                <li>Spaghetti alla Carbonara</li>
-                <li>Spaghetti al Pesto</li>
-                <li>Gnocchi al Gorgonzola</li>
-              </ol>
-            </div>
-            <div className="bg-blue-200 p-2">
-              <h2 className="p-2 text-xl">Secondi</h2>
-              <ol>
-                <li>Frittura di Calamari</li>
-                <li>Fritto misto</li>
-                <li>Grigleata mista</li>
-                <li>Branzino alla Ligure</li>
-                <li>Branzino all griglia</li>
-                <li>Orata con Patate e Porcini</li>
-                <li>Orata alla griglia</li>
-                <li>Gamberoni alla griglia</li>
-                <li>Milanese</li>
-                <li>Scaloppina a scelta</li>
-              </ol>
-            </div>
-            <div>
-                <h2 className="p-2 text-xl">Dessert</h2>
-                <ol>
-                    <li>Creme Caramel</li>
-                    <li>Tiramisu</li>
-                    <li>Panna cotta</li>
-                    <li>Millefoglie con Crema, Panna e Fragole</li>
-                    <li>Torta alla Frutta</li>
-                    <li>Crema Catalana</li>
-                    <li>Gelato</li>
-                    <li>Sorbetto</li>
-                    <li>Crepes con le Mele</li>
-                    <li>Tortino al Cioccolato</li>
-                </ol>
-            </div>
-          </div>
-        </div>
+        <h1 className='text-4xl text-center'>Menu</h1>
+        
+      <div className={styles.main}>
+        <IndexParallax />
+        <IndexParallax2 />
+        <IndexParallax3 />
+      </div>
+
         {/* vista responsive */}
+        
         <div></div>
       </main>
       <Footer />
     </div>
   );
 }
+
+
+
+
